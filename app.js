9010528c8da2b3da333b467d195f0172459d88e6
Nir Tamir <nirtamir2@gmail.com>
2017-07-01 10:23:37 +0300

    add firebase integration

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express')
 var app = express()
 var path = require("path");
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./private/stairs-bd20d-firebase-adminsdk-xwaua-b513215370.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://stairs-bd20d.firebaseio.com/"
+});
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -8,8 +16,17 @@ app.get('/', function (req, res) {
 
 app.use(express.static(__dirname + '/static'));
 
-var i = 0;
 
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("/enter");
+ref.once("value", function(snapshot) {
+    console.log(snapshot.val());
+});
+
+
+
+var i = 0;
 app.get('/floor/:floorNumber', function (req, res) {
     var ip = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
@@ -18,11 +35,18 @@ app.get('/floor/:floorNumber', function (req, res) {
 
     i++;
 
+    var enterRef = db.ref("/enter");
+    var newEnter = enterRef.push();
+    newEnter.set({
+        ip: ip,
+        floor: req.params.floorNumber,
+        timestamp: Date.now()
+    });
 
-    console.log(i)
-    console.log(ip)
+    console.log(i);
+    console.log(ip);
 
-    res.send(ip)
+    res.send(ip);
 });
 
 app.listen(8081)
